0fa9627b77f012059e06410fa36e425bd127eae9
komi <akomilul@gmail.com>
2024-01-25 22:43:27 +0500

    Minor changes
    In dunst, swayidle.sh, keybindings

--- scripts/swayidle.sh
+++ scripts/swayidle.sh
@@ -5,4 +5,7 @@ swayidle -w timeout 300 'swaylock -f' \
             timeout 480 'hyprctl dispatch dpms off' \
             resume 'hyprctl dispatch dpms on' \
             timeout 900 'systemctl suspend' \
-            before-sleep 'swaylock -f' &
+            after-resume 'swaylock -f'
+
+# NOTE: "before-sleep" was substitued with "after-resume"
+# due to a bug in swaylock package(?): high sound through the speaker
