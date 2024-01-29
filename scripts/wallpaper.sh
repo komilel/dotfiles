5781e6e426988b37f9bf04a4b9bb2fbac3acd74d
komi <akomilul@gmail.com>
2024-01-29 16:04:11 +0500

    New: Rofi default conf, wal integration Also added wal templates

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+#                _ _                                 _     
+# __      ____ _| | |_ __   __ _ _ __   ___ _ __ ___| |__  
+# \ \ /\ / / _` | | | '_ \ / _` | '_ \ / _ \ '__/ __| '_ \ 
+#  \ V  V / (_| | | | |_) | (_| | |_) |  __/ | _\__ \ | | |
+#   \_/\_/ \__,_|_|_| .__/ \__,_| .__/ \___|_|(_)___/_| |_|
+#                   |_|         |_|                        
 # Wallpaper script for Hyprland with Pywal colors
 
 # Caching file for current wallpaper
 cache_wallpaper="$HOME/.cache/current_wallpaper"
-# rasi_file = ...
+rasi_file="$HOME/.cache/current_wallpaper.rasi"
 
 # Get random wallpaper from the wallpapers folder
 random_wallpaper=$(find ~/wallpapers -type f | shuf -n 1)
@@ -12,12 +18,15 @@ random_wallpaper=$(find ~/wallpapers -type f | shuf -n 1)
 case $1 in
     "random")
         # Wallpaper initialisation on hyprland startup or random wallpaper change
-        wal -n -i "$random_wallpaper"
+        wal -a 1.0 -n -i "$random_wallpaper"
     ;;
 esac
 
 # Cache wallpaper
 echo "$random_wallpaper" > "$cache_wallpaper"
+
+# Cache wallpaper in rasi
+echo "* { current-wallpaper: url(\"$random_wallpaper\", height); }" > "$rasi_file"
 
 # Change wallpaper with swww
 swww img "$random_wallpaper" \
