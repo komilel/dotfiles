fd01a6f77ee2043e2ea19ba2eebc8c48afe55271
komi <akomilul@gmail.com>
2024-02-23 11:47:23 +0500

    Feat. Wallpaper select via rofi

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-#                _ _                                 _     
-# __      ____ _| | |_ __   __ _ _ __   ___ _ __ ___| |__  
-# \ \ /\ / / _` | | | '_ \ / _` | '_ \ / _ \ '__/ __| '_ \ 
-#  \ V  V / (_| | | | |_) | (_| | |_) |  __/ | _\__ \ | | |
-#   \_/\_/ \__,_|_|_| .__/ \__,_| .__/ \___|_|(_)___/_| |_|
-#                   |_|         |_|                        
+# __        __    _ _                                 _     
+# \ \      / /_ _| | |_ __   __ _ _ __   ___ _ __ ___| |__  
+#  \ \ /\ / / _` | | | '_ \ / _` | '_ \ / _ \ '__/ __| '_ \ 
+#   \ V  V / (_| | | | |_) | (_| | |_) |  __/ | _\__ \ | | |
+#    \_/\_/ \__,_|_|_| .__/ \__,_| .__/ \___|_|(_)___/_| |_|
+#                    |_|         |_|                        
+#
 # Wallpaper script for Hyprland with Pywal colors
 
 # Caching file for current wallpaper
 cache_wallpaper="$HOME/.cache/current_wallpaper"
 rasi_file="$HOME/.cache/current_wallpaper.rasi"
-
-# Get random wallpaper from the wallpapers folder
-random_wallpaper=$(find ~/wallpapers -type f | shuf -n 1)
 
 case $1 in
     "init")
@@ -21,32 +19,74 @@ case $1 in
         if [ -f "$cache_wallpaper" ]; then
             wal -a 1.0 -n -i "$cache_wallpaper"
             swww img "$cache_wallpaper" \
-                --transition-type=random \
+                --transition-type="outer" \
                 --transition-fps=60 \
                 --transition-duration=1
         fi
+
         exit 0
+    ;;
+
+    "select")
+        selected=$( find "$HOME/wallpapers" -type f -exec basename {} \; | while read wall
+        do
+            echo -en "$wall\x00icon\x1f$HOME/wallpapers/${wall}\n"
+        done | rofi -dmenu -i -replace -config ~/dotfiles/rofi/wallpaper.rasi )
+            
+        if [ ! "$selected" ]; then
+            echo "No wallpaper's selected"
+            exit 0
+        fi
+        
+        # Change color scheme
+        wal -a 1.0 -n -i "$HOME/wallpapers/$selected"
+
+        # Cache wallpaper
+        echo "$HOME/wallpapers/$selected" > "$cache_wallpaper"
+
+        # Cache wallpaper in rasi
+        echo "* { current-wallpaper: url(\"$HOME/wallpapers/$selected\", height); }" > "$rasi_file"
+
+        # Change wallpaper with swww
+        swww img "$HOME/wallpapers/$selected" \
+                --transition-type="outer" \
+                --transition-fps=60 \
+                --transition-duration=1.5
+
+        # Relaunch waybar with new colors
+        killall waybar && waybar
+
+        # Reload xray feature in hyprland
+        hyprctl reload
+
+        exit 0    
     ;;        
+
     "random")
+        # Get random wallpaper from the wallpapers folder
+        random_wallpaper=$(find ~/wallpapers -type f | shuf -n 1)
+
         # Random wallpaper change
         wal -a 1.0 -n -i "$random_wallpaper"
+        
+        # Cache wallpaper
+        echo "$random_wallpaper" > "$cache_wallpaper"
+
+        # Cache wallpaper in rasi
+        echo "* { current-wallpaper: url(\"$random_wallpaper\", height); }" > "$rasi_file"
+
+        # Change wallpaper with swww
+        swww img "$random_wallpaper" \
+                --transition-type="outer" \
+                --transition-fps=60 \
+                --transition-duration=1.5
+
+        # Relaunch waybar with new colors
+        killall waybar && waybar
+
+        # Reload xray feature in hyprland
+        hyprctl reload
+
+        exit 0
     ;;
 esac
-
-# Cache wallpaper
-echo "$random_wallpaper" > "$cache_wallpaper"
-
-# Cache wallpaper in rasi
-echo "* { current-wallpaper: url(\"$random_wallpaper\", height); }" > "$rasi_file"
-
-# Change wallpaper with swww
-swww img "$random_wallpaper" \
-        --transition-type="outer" \
-        --transition-fps=60 \
-        --transition-duration=1.5
-
-# Relaunch waybar with new colors
-killall waybar && waybar
-
-# Reload xray feature in hyprland
-hyprctl reload
